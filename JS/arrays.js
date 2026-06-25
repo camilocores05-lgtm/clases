4be7851000e116aad94d5dd21a7b832b37e5feac
camilocores05-lgtm <camilocores05@gmail.com>
2026-06-24 21:17:26 -0300

    Eliminar archivos antiguos y dejar solo act17

--- JS/arrays.js
+++
@@ -1,5 +0,0 @@
-// Metodos de arrays
-// la mayoria de tipos de dato poseen funciones internas
-// herramientas para trabajr con los tipos de dato
-
-//forEach -> por cada uno
--- JS/pop.js
+++
@@ -1,8 +0,0 @@
-// POP elimina el ultimo elemento del array
-
-// EJ1 boton desahcer. tienes un array de acciones con  "escribir" "borrar" "pintar". deshaz la ultima acciony  muestra que accion se deshizo
-
-let acciones = ["escribir", "borrar", "pintar"]
-console.log(acciones);
-console.log(acciones.pop())
-console.log(acciones);
