116a76130de2da1630f10f68c192f1cb840a1db4
camilocores05-lgtm <camilocores05@gmail.com>
2026-06-24 21:23:18 -0300

    eliminar clases

--- act17/class21/calculadora1/src/components/input.jsx
+++
@@ -1,8 +0,0 @@
-export function input({type, label, onChange, value, labaleId}){
-    return(
-        <div>
-            <label htmlFor="{labelId">Texto del label </label>
-            <input type={type} value={value} onChange={onChange} id={labelId} />
-        </div>
-    )
-}
